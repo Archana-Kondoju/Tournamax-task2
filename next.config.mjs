dce5068f7f39db7e9c1be769ef43f508d3ae6078
archana-kondoju2 <archanakondoju2@email.com>
2024-08-16 14:18:36 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true, // Enables React's Strict Mode for highlighting potential problems
-    swcMinify: true, // Enables minification using SWC
-    experimental: {
-      appDir: true, // Use experimental app directory feature (if applicable)
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
