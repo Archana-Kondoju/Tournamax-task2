3734441280a516230101d94ceace09ae4b212396
archana-kondoju2 <archanakondoju2@email.com>
2024-08-16 15:26:45 +0530

    updated

--- src/app/map/Map.tsx
+++ src/app/map/Map.tsx
@@ -1,29 +1,31 @@
+"use client"
+import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
 
-"use client";
-
-import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import L, { LatLngTuple } from 'leaflet';
 
-let DefaultIcon = L.icon({
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
 
-// Override the default icon for all markers
-L.Marker.prototype.options.icon = DefaultIcon;
+// Set default icon for markers
+
+  const DefaultIcon = L.icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
+
+  L.Marker.prototype.options.icon = DefaultIcon;
+
 
 function Map() {
-  const markers = [
-    { position: [28.6139, 77.2090] as [number, number], label: "New Delhi" },
-    { position: [19.0760, 72.8777] as [number, number], label: "Mumbai" },
-    { position: [13.0827, 80.2707] as [number, number], label: "Chennai" },
-    { position: [22.5726, 88.3639] as [number, number], label: "Kolkata" },
-    { position: [12.9716, 77.5946] as [number, number], label: "Bangalore" },
+  const markers: { position: LatLngTuple; label: string }[] = [
+    { position: [28.6139, 77.2090], label: "New Delhi" },
+    { position: [19.0760, 72.8777], label: "Mumbai" },
+    { position: [13.0827, 80.2707], label: "Chennai" },
+    { position: [22.5726, 88.3639], label: "Kolkata" },
+    { position: [12.9716, 77.5946], label: "Bangalore" },
   ];
 
   return (
